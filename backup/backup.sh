91e5ee4b5e50a2c76e45368baf7f4e936249a105
pro7357 <pro7357@protonmail.com>
2022-08-07 23:50:44 +0800

    Initial commit for backup

---
+++ backup/backup.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/bash
+_help(){ cat <<E0F
+Usage: default [OPTION]... [FILE]...
+Do default thing to FILE(s). Output to standard output.
+
+With no FILE, or when FILE is -, read standard input.
+
+  -q, --quiet    do not print anything to stdout
+  -v, --verbose  increase verbosity
+  -h, --help     display this help and exit
+E0F
+}
+
+_main(){
+    echo "This is main"
+}
+
+_menu(){
+    #see notdefault.sh for more complex options.
+    if [[ $1 == '-h' || $1 == --help ]]; then
+        _help
+    elif [[ "${@:(-1)}" == '-' ]]; then
+        echo "see _stdin in notdefault.sh"
+    elif [[ -z $1 ]]; then
+        _main
+    else
+        #_main "$@"
+        echo "$@"
+    fi
+}
+_source(){ :;}
+[[ $0 == "$BASH_SOURCE" ]] && _menu "$@" || _source
