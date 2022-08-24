7c9dfb1a18b285562ff577405a0ec25065d3c7c1
pro7357 <pro7357@protonmail.com>
2022-08-24 17:37:42 +0800

    Update backup

--- backup/backup.sh
+++ backup/backup.sh
@@ -21,7 +21,8 @@ Options:
 Status: This is an ongoing remake.
         Also experimenting with .env
 - [x] Full system backup to live ssd
-- [ ] Basic backup, to be run daily
+- [x] Basic backup, to be run daily
+- [ ] Full system backup to live external canvio
 E0F
 }
 
@@ -62,35 +63,51 @@ _main(){
     verify_ssd
 
     if [[ $1 == 'full' ]]; then
-        full_system
+        full_system2ssd
     elif [[ -z $1 ]]; then
-        basic_backup
+        #basic_backup
+        basic_system2ssd
     else
         status="Unknown input: $@"
         if $verbose; then echo "$status"; fi
     fi
 }
 
-basic_backup(){
+#https://wiki.archlinux.org/title/Rsync#As_a_backup_utility
+# /dev/*            # only dir "dev" and not content. dev will be populated on boot.
+# "/home/data"      # will totally ignore /home/data
+#        --include "/home/private" \    # include first
+#        --exclude "/home/*" \          # then exclude
+
+basic_system2ssd(){
     sudo rsync -vaHAXS --delete \
         --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found"} \
         --exclude={"/etc/fstab","/etc/default/grub","/boot/grub/grub.cfg"} \
+        --exclude={"/home/data","/home/backup"} \
         --exclude={"/home/*/.data","/home/*/.tmp"} \
-        --exclude={"/home/d/music/*","/home/d/musics/*"} \
-        --exclude "/home/d/qemu/*" \
-        --exclude={"/home/*/.cache/chromium","/home/*/.config/chromium","/home/d/.local/opt/tor-browser/*"} \
+        --exclude={"/home/d/music","/home/d/musics"} \
+        --exclude={"/home/d/qemu","/var/lib/libvirt/images"} \
+        --exclude={"/home/*/.cache/chromium","/home/*/.config/chromium","/home/d/.local/opt/tor-browser"} \
         --exclude "/home/*/.cache/mesa_shader_cache" \
         / /media/arch
 }
-
-full_system(){
+# full except qemu image and music
+full_system2ssd(){
     sudo rsync -vaHAXS --delete \
         --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found"} \
         --exclude={"/etc/fstab","/etc/default/grub","/boot/grub/grub.cfg"} \
-        --exclude={"/home/*/.data","/home/*/.tmp"} \
+        --exclude={"/home/data","/home/backup"} \
         --exclude={"/home/d/music/*","/home/d/musics/*"} \
-        --exclude "/home/d/qemu/*" \
+        --exclude={"/home/d/qemu","/var/lib/libvirt/images"} \
         / /media/arch
+}
+
+full_system2canvio(){
+    sudo rsync -vaHAXS --delete \
+        --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found"} \
+        --exclude={"/etc/fstab","/etc/default/grub","/boot/grub/grub.cfg"} \
+        --exclude={"/home/data","/home/backup"} \
+        / /media/canvio
 }
 
 verify_ssd(){
@@ -98,8 +115,7 @@ verify_ssd(){
     if [[ -e /dev/disk/by-uuid/$uuid_ssd && \
         -f /media/arch/home/d/.bashrc && \
         -e /dev/disk/by-uuid/$uuid_nvme && \
-        -f /home/d/.bashrc ]]
-    then
+        -f /home/d/.bashrc ]]; then
         return 0
     else
         status="Error: ssd not found"
